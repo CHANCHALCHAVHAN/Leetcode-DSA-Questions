644c2eb49fb82ada47fbfb0e666bdf936ed364fa
Chanchal Chavhan <143700585+CHANCHALCHAVHAN@users.noreply.github.com>
2025-12-10 21:00:59 +0530

    Update 1) Two Sum.cpp

--- 1) Two Sum.cpp
+++ 1) Two Sum.cpp
@@ -35,6 +35,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         for(int i=0;i<nums.size();i++){
+         //create the pairs of i and j
             for(int j=i+1;j<nums.size();j++){
                 if((nums[i]+nums[j])==target){
                     return {i,j};
