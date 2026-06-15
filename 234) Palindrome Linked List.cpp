32ab50923f6ea6426e11ef910d0a586effe66566
Chanchal Chavhan <143700585+CHANCHALCHAVHAN@users.noreply.github.com>
2026-06-15 09:15:32 +0530

    🚀🚀Add isPalindrome function for linked list
    Implement palindrome check for singly linked list.

---
+++ 234) Palindrome Linked List.cpp
@@ -0,0 +1,49 @@
+/*
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,2,1]
+Output: true
+Example 2:
+
+
+Input: head = [1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        string values = "";
+        ListNode* current = head;
+
+        while(current != nullptr){
+            values += current-> val;
+            current = current-> next;
+        }
+        string og_values = values;
+        reverse(values.begin() , values.end());
+        
+        if(og_values == values) return true;
+        return false;
+    }
+};
